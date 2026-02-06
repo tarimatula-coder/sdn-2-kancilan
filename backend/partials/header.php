3f41e3f48f8a22734bba540ae867a737196b4a1d
tarimatula-coder <tarimatula@gmail.com>
2026-02-06 22:28:43 +0700

    template_admin

---
+++ backend/partials/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elegant Dashboard | Dashboard</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../elegant/./img/svg/logo.svg" type="image/x-icon">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="../elegant/./css/style.min.css">
+</head>
+
+<body>
---
+++ backend/partials/script.php
@@ -0,0 +1,9 @@
+<!-- Chart library -->
+<script src="../elegant/./plugins/chart.min.js"></script>
+<!-- Icons library -->
+<script src="../elegant/plugins/feather.min.js"></script>
+<!-- Custom scripts -->
+<script src="../elegant/js/script.js"></script>
+</body>
+
+</html>
