a57fe90ebaf78b0dd436dbc13bb46b78aada1607
tarimatula-coder <tarimatula@gmail.com>
2026-02-06 20:42:57 +0700

    config

---
+++ config/connection.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "sdn_2_kancilan";
+
+
+$connect = mysqli_connect($hostname, $username, $password, $database);
+if (!$connect) {
+    die("koneksi gagal: " . mysqli_connect_error());
+}
---
+++ config/escapeString.php
@@ -0,0 +1,6 @@
+<?php
+function escapeString($text)
+{
+    global $connect;
+    return $connect->real_escape_string($text);
+}
