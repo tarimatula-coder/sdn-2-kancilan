bf28faee355ec43d220b37c892ed314f628433fa
tarimatula-coder <tarimatula@gmail.com>
2026-02-07 07:22:40 +0700

    partials

--- backend/partials/header.php
+++ backend/partials/header.php
@@ -2,14 +2,38 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elegant Dashboard | Dashboard</title>
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../elegant/./img/svg/logo.svg" type="image/x-icon">
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="../elegant/./css/style.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Admin Dashboard</title>
+
+    <!-- AdminKit CSS -->
+    <link href="../../adminkit-3.1.0/adminkit-3.1.0/static/css/app.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <!-- FIX SPASI DASHBOARD -->
+    <style>
+        /* hilangkan spasi atas konten */
+        .content {
+            padding-top: 0.5rem !important;
+        }
+
+        /* hilangkan margin default body */
+        body {
+            margin: 0;
+        }
+
+        /* pastikan layout full */
+        .wrapper {
+            min-height: 100vh;
+        }
+
+        /* rapikan navbar */
+        .navbar {
+            margin-bottom: 0 !important;
+        }
+    </style>
 </head>
 
 <body>
--- backend/partials/sidebar.php
+++ backend/partials/sidebar.php
@@ -1,154 +1,106 @@
- <aside class="sidebar">
-     <div class="sidebar-start">
-         <div class="sidebar-head">
-             <a href="/" class="logo-wrapper" title="Home">
-                 <span class="sr-only">Home</span>
-                 <span class="icon logo" aria-hidden="true"></span>
-                 <div class="logo-text">
-                     <span class="logo-title">Elegant</span>
-                     <span class="logo-subtitle">Dashboard</span>
-                 </div>
 
-             </a>
-             <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
-                 <span class="sr-only">Toggle menu</span>
-                 <span class="icon menu-toggle" aria-hidden="true"></span>
-             </button>
-         </div>
-         <div class="sidebar-body">
-             <ul class="sidebar-body-menu">
-                 <li>
-                     <a class="active" href="/"><span class="icon home" aria-hidden="true"></span>Dashboard</a>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon document" aria-hidden="true"></span>Posts
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="posts.html">All Posts</a>
-                         </li>
-                         <li>
-                             <a href="new-post.html">Add new post</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon folder" aria-hidden="true"></span>Categories
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="categories.html">All categories</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon image" aria-hidden="true"></span>Media
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="media-01.html">Media-01</a>
-                         </li>
-                         <li>
-                             <a href="media-02.html">Media-02</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon paper" aria-hidden="true"></span>Pages
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="pages.html">All pages</a>
-                         </li>
-                         <li>
-                             <a href="new-page.html">Add new page</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a href="comments.html">
-                         <span class="icon message" aria-hidden="true"></span>
-                         Comments
-                     </a>
-                     <span class="msg-counter">7</span>
-                 </li>
-             </ul>
-             <span class="system-menu__title">system</span>
-             <ul class="sidebar-body-menu">
-                 <li>
-                     <a href="appearance.html"><span class="icon edit" aria-hidden="true"></span>Appearance</a>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon category" aria-hidden="true"></span>Extentions
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="extention-01.html">Extentions-01</a>
-                         </li>
-                         <li>
-                             <a href="extention-02.html">Extentions-02</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a class="show-cat-btn" href="##">
-                         <span class="icon user-3" aria-hidden="true"></span>Users
-                         <span class="category__btn transparent-btn" title="Open list">
-                             <span class="sr-only">Open list</span>
-                             <span class="icon arrow-down" aria-hidden="true"></span>
-                         </span>
-                     </a>
-                     <ul class="cat-sub-menu">
-                         <li>
-                             <a href="users-01.html">Users-01</a>
-                         </li>
-                         <li>
-                             <a href="users-02.html">Users-02</a>
-                         </li>
-                     </ul>
-                 </li>
-                 <li>
-                     <a href="##"><span class="icon setting" aria-hidden="true"></span>Settings</a>
-                 </li>
-             </ul>
-         </div>
-     </div>
-     <div class="sidebar-footer">
-         <a href="##" class="sidebar-user">
-             <span class="sidebar-user-img">
-                 <picture>
-                     <source srcset="./img/avatar/avatar-illustrated-01.webp" type="image/webp"><img src="./img/avatar/avatar-illustrated-01.png" alt="User name">
-                 </picture>
-             </span>
-             <div class="sidebar-user-info">
-                 <span class="sidebar-user__title">Nafisa Sh.</span>
-                 <span class="sidebar-user__subtitle">Support manager</span>
-             </div>
-         </a>
-     </div>
- </aside>
+    <nav id="sidebar" class="sidebar js-sidebar">
+        <div class="sidebar-content js-simplebar">
+            <a class="sidebar-brand" href="index.html">
+                <span class="align-middle">AdminKit</span>
+            </a>
+
+            <ul class="sidebar-nav">
+                <li class="sidebar-header">
+                    Pages
+                </li>
+
+                <li class="sidebar-item active">
+                    <a class="sidebar-link" href="index.html">
+                        <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-profile.html">
+                        <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-in.html">
+                        <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-up.html">
+                        <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-blank.html">
+                        <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-header">
+                    Tools & Components
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-buttons.html">
+                        <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-forms.html">
+                        <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-cards.html">
+                        <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="ui-typography.html">
+                        <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="icons-feather.html">
+                        <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-header">
+                    Plugins & Addons
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="charts-chartjs.html">
+                        <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="maps-google.html">
+                        <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="sidebar-cta">
+                <div class="sidebar-cta-content">
+                    <strong class="d-inline-block mb-2">Upgrade to Pro</strong>
+                    <div class="mb-3 text-sm">
+                        Are you looking for more components? Check out our premium version.
+                    </div>
+                    <div class="d-grid">
+                        <a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
